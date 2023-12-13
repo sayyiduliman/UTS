bc7c1447fa3abaaa8a4ef4288080425d2881c3d0
sayyiduliman <saynashcloth@gmail.com>
2023-12-13 20:58:23 +0700

    update

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function toggleInfo(paragraphId, buttonId) {
     var paragraph = document.getElementById(paragraphId);
     var button = document.getElementById(buttonId);
 
-    if (paragraph.style.display === 'none' || paragraph.style.display === '') {
+    var displayStyle = window.getComputedStyle(paragraph).getPropertyValue('display');
+
+    if (displayStyle === 'none' || displayStyle === '') {
         paragraph.style.display = 'block';
         button.textContent = 'Less Info';
     } else {
@@ -10,3 +12,22 @@ function toggleInfo(paragraphId, buttonId) {
         button.textContent = 'More Info';
     }
 }
+
+function tambah() {
+    var inputElement = document.getElementById("email2");
+    var inputElement2 = document.getElementById("subject2");
+    var inputElement3 = document.getElementById("message2");
+    var inputValue = inputElement.value.trim();
+    var inputValue2 = inputElement2.value.trim();
+    var inputValue3 = inputElement3.value.trim();
+    if (inputValue != "" && inputValue2 != "" && inputValue3 != "") {
+      alert("Form Submitted");
+      inputElement.value = "";
+      inputElement2.value = "";
+      inputElement3.value = "";
+  
+      // location.reload();
+    } else {
+      alert("Mohon isi formulirnya dengan lengkap");
+    }
+  }
